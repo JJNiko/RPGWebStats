070ae68462e03be2be82d9d96aa7478bc27383d1
JJNiko <128691300+JJNiko@users.noreply.github.com>
2026-06-19 23:36:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 export default defineConfig({
   base: '/RPGWebStats/',
-  plugins: [inspectAttr(), react()],
+  plugins: [react()],
   server: {
     port: 3000,
   },
